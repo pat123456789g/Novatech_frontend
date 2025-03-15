d5458e27b5aa1b849df177b9bd525c8017ec5515
Patrick Mutua <152162920+pat123456789g@users.noreply.github.com>
2025-03-15 09:54:15 +0300

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import toast from "react-hot-toast";
-import api from 'https://github.com/georges-mars/Novatech/blob/main/src/api';
+import api from 'https://github.com/pat123456789g/Novatech_frontend/blob/main/src/api';
 
 const Contact = () => {
   const [formData, setFormData] = useState({
