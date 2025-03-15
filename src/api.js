b5471a70ae07e013fe814b155d612bb53c8df1c5
georges mars <georgesmars8@gmail.com>
2025-03-15 09:28:43 +0300

    first commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000', // This points to your Django backend
+  baseURL: 'https://novatech-backend-9kqw.onrender.com', // This points to your Django backend
+ // baseURL: 'http://localhost:8000', // This points to your Django backend
   headers: {
     'Content-Type': 'application/json',
   },
